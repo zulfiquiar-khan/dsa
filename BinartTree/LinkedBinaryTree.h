80cd72d927d3372dffcff0a286aaff0c7335b852
Zulfiquiar <faiziykhan@gmail.com>
2015-09-04 23:38:12 +0530

    Binary Tree and a methode to create a binary tree

---
+++ BinartTree/LinkedBinaryTree.h
@@ -0,0 +1,74 @@
+#include<stdlib.h>
+
+struct BinaryTreeNode{
+    int data;
+    struct BinaryTreeNode* left;
+    struct BinaryTreeNode* right;
+};
+
+typedef struct BinaryTreeNode node;
+
+node* createBinaryTree(int size){
+    node* root=(node*)malloc(sizeof(node));
+    int i=1;
+    if(root==NULL){
+        printf("Memory error\n");
+        return NULL;
+    }
+    root->data=i;
+    root->left=NULL;
+    root->right=NULL;
+    i++;
+    int flag=0;
+    while(i<=size){
+        printf("inserting %d \n",i);
+        printf("----------------------\n");
+        insert(root,i,flag);
+        printf("inserted %d \n",i);
+        printf("----------------------\n");
+        if(flag==0){flag=1;}
+        else{flag=0;}
+        i++;
+    }
+    return root;
+}
+
+void insert(node* root,int data,int flag){
+    node* newNode=(node*)malloc(sizeof(node));
+    newNode->data=data;
+    newNode->left=NULL;
+    newNode->right=NULL;
+    while(root!=NULL){
+        if(root->left==NULL && root->right==NULL){
+            if(flag==0){
+                root->left=newNode;
+                printf("inserted in left of %d \n",root->data);
+                }
+            else{
+                root->right=newNode;
+                printf("inserted in right of %d\n",root->data);
+            }
+            break;
+        }
+        else if(root->left==NULL){
+            root->left=newNode;
+            printf("inserted in left of %d\n",root->data);
+            break;
+        }
+        else if(root->right==NULL){
+            root->right=newNode;
+            printf("inserted in right of %d\n",root->data);
+            break;
+        }
+        else{
+            if(flag==0){
+                root=root->left;
+                printf("moved to left\n");
+            }
+            if(flag==1){
+                root=root->right;
+                printf("moved to right\n");
+            }
+        }
+    }
+}
---
+++ BinartTree/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include"LinkedBinaryTree.h"
+
+int main(int argc, char **argv)
+{
+	node* binaryTree=createBinaryTree(7);
+	return 0;
+}
