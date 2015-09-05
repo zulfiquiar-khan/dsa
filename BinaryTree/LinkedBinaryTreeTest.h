83f2fa19a2b0c91483ff496291195c6e880d9597
Zulfiquiar <faiziykhan@gmail.com>
2015-09-05 12:48:02 +0530

    Postorder iterative implementation

---
+++ BinaryTree/LinkedBinaryTreeTest.h
@@ -0,0 +1,49 @@
+#include"LinkedBinaryTree.h"
+
+void testCreateBinaryTree(){
+    	node* binaryTree=createBinaryTree(7);
+}
+
+void testPreOrderTraversalRecursive(){
+    node* root=createBinaryTree(7);
+    printf("PreOrder : ");
+    preOrderTraversalRecursive(root);
+    printf("\n");
+}
+
+void testPreOrderTraversalIterative(){
+    node* root=createBinaryTree(7);
+    printf("PreOrder : ");
+    preOrderTraversalIterative(root);
+    printf("\n");
+}
+
+
+void testInOrderTraversalRecursive(){
+    node* root=createBinaryTree(7);
+    printf("InOrder : ");
+   inOrderTraversalRecursive(root);
+    printf("\n");
+}
+
+
+void testInOrderTraversalIterative(){
+    node* root=createBinaryTree(7);
+    printf("InOrder : ");
+    inOrderTraversalIterative(root);
+    printf("\n");
+}
+
+void testPostOrderTraversalRecursive(){
+    node* root=createBinaryTree(7);
+    printf("PostOrder : ");
+    postOrderTraversalRecursive(root);
+    printf("\n");
+}
+
+void testPostOrderTraversalIterative(){
+    node* root=createBinaryTree(7);
+    printf("PostOrder : ");
+    postOrderTraversalIterative(root);
+    printf("\n");
+}
---
+++ BinaryTree/main.c
@@ -0,0 +1,15 @@
+#include"LinkedBinaryTreeTest.h"
+
+int main(int argc, char **argv)
+{
+
+    //testCreateBinaryTree();
+    //testPreOrderTraversalRecursive();
+	//testPreOrderTraversalIterative();
+    //testInOrderTraversalRecursive();
+    //testInOrderTraversalIterative();
+    //testPostOrderTraversalRecursive();
+    testPostOrderTraversalIterative();
+    
+    return 0;
+}
